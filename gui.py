56bd3fcfad36a5564e59fb3120519dd3901f48cf
Abdellah <abdelahe53@gmail.com>
2024-08-23 16:27:05 +0100

    new1

--- gui.py
+++ gui.py
@@ -4,13 +4,13 @@ root = Tk()
 
 e = Entry(root, width=50, bg="green", fg="white", borderwidth=5)
 e.pack()
-e.get()
-
+e.insert(0, "Write something: ")
 def myClick():
-    mylable = Label(root, text=e.get())
+    hello = "This is what you typed: " + e.get()
+    mylable = Label(root, text=hello)
     mylable.pack()
 
-mybutton = Button(root, text="enter your name", command=myClick)
+mybutton = Button(root, text="Click!", command=myClick)
 mybutton.pack()
 
 root.mainloop()
